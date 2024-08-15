de289db7fb1af07cd173f0924c12a1870737458e
Danie Marques de Almeida <danielmarques.enfermagem@gmail.com>
2024-08-15 01:42:18 -0300

    Atualizar 2 app.py

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import streamlit as st
-import pandas as pd
 import os
 from sqlalchemy import create_engine, Column, Integer, String, Text
 from sqlalchemy.exc import SQLAlchemyError
@@ -19,8 +18,9 @@ DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_DATABASE}"
 # Definindo a base e a tabela usando SQLAlchemy ORM
 Base = declarative_base()
 
+
 class SurveyData(Base):
-    __tablename__ = 'survey_data'
+    __tablename__ = "survey_data"
     id = Column(Integer, primary_key=True, autoincrement=True)
     estado = Column(String(50))
     bibliotecas = Column(Text)
@@ -31,6 +31,7 @@ class SurveyData(Base):
     experiencia_sql = Column(Integer)
     experiencia_cloud = Column(Integer)
 
+
 def get_engine():
     try:
         engine = create_engine(DATABASE_URL)
@@ -39,12 +40,14 @@ def get_engine():
         st.error(f"Erro ao conectar ao banco de dados: {e}")
         return None
 
+
 # Função para criar a tabela caso não exista
 def criar_tabela_se_nao_existir(engine):
     try:
         Base.metadata.create_all(engine)
     except SQLAlchemyError as e:
         st.error(f"Erro ao criar a tabela: {e}")
+
 
 # Função para salvar dados no banco de dados
 def salvar_dados_banco(session, dados):
@@ -65,6 +68,7 @@ def salvar_dados_banco(session, dados):
         st.error(f"Erro ao salvar os dados no banco de dados: {e}")
         session.rollback()
 
+
 # Obter a instância do engine e criar a tabela se necessário
 engine = get_engine()
 if engine is not None:
@@ -74,18 +78,60 @@ if engine is not None:
 Session = sessionmaker(bind=engine)
 
 # Opções de estados, áreas de atuação, bibliotecas, horas codando e conforto com dados
-estados = ["Acre", "Alagoas", "Amapá", "Amazonas", "Bahia", "Ceará",
-           "Distrito Federal", "Espírito Santo", "Goiás", "Maranhão",
-           "Mato Grosso", "Mato Grosso do Sul", "Minas Gerais", "Pará",
-           "Paraíba", "Paraná", "Pernambuco", "Piauí", "Rio de Janeiro",
-           "Rio Grande do Norte", "Rio Grande do Sul", "Rondônia", "Roraima",
-           "Santa Catarina", "São Paulo", "Sergipe", "Tocantins"]
+estados = [
+    "Acre",
+    "Alagoas",
+    "Amapá",
+    "Amazonas",
+    "Bahia",
+    "Ceará",
+    "Distrito Federal",
+    "Espírito Santo",
+    "Goiás",
+    "Maranhão",
+    "Mato Grosso",
+    "Mato Grosso do Sul",
+    "Minas Gerais",
+    "Pará",
+    "Paraíba",
+    "Paraná",
+    "Pernambuco",
+    "Piauí",
+    "Rio de Janeiro",
+    "Rio Grande do Norte",
+    "Rio Grande do Sul",
+    "Rondônia",
+    "Roraima",
+    "Santa Catarina",
+    "São Paulo",
+    "Sergipe",
+    "Tocantins",
+]
 
 areas_atuacao = ["Analista de Dados", "Cientista de Dados", "Engenheiro de Dados"]
 
-bibliotecas = ["Pandas", "Pydantic", "scikit-learn", "Git", "Pandera", "streamlit",
-               "postgres", "databricks", "AWS", "Azure", "airflow", "dbt",
-               "Pyspark", "Polars", "Kafka", "Duckdb", "PowerBI", "Excel", "Tableau", "storm"]
+bibliotecas = [
+    "Pandas",
+    "Pydantic",
+    "scikit-learn",
+    "Git",
+    "Pandera",
+    "streamlit",
+    "postgres",
+    "databricks",
+    "AWS",
+    "Azure",
+    "airflow",
+    "dbt",
+    "Pyspark",
+    "Polars",
+    "Kafka",
+    "Duckdb",
+    "PowerBI",
+    "Excel",
+    "Tableau",
+    "storm",
+]
 
 horas_codando = ["Menos de 5", "5-10", "10-20", "Mais de 20"]
 
@@ -95,9 +141,13 @@ conforto_dados = ["Desconfortável", "Neutro", "Confortável", "Muito Confortáv
 with st.form("dados_enquete"):
     estado = st.selectbox("Estado", estados)
     area_atuacao = st.selectbox("Área de Atuação", areas_atuacao)
-    bibliotecas_selecionadas = st.multiselect("Bibliotecas e ferramentas mais utilizadas", bibliotecas)
+    bibliotecas_selecionadas = st.multiselect(
+        "Bibliotecas e ferramentas mais utilizadas", bibliotecas
+    )
     horas_codando = st.selectbox("Horas Codando ao longo da semana", horas_codando)
-    conforto_dados = st.selectbox("Conforto ao programar e trabalhar com dados", conforto_dados)
+    conforto_dados = st.selectbox(
+        "Conforto ao programar e trabalhar com dados", conforto_dados
+    )
     experiencia_python = st.slider("Experiência de Python", 0, 10)
     experiencia_sql = st.slider("Experiência de SQL", 0, 10)
     experiencia_cloud = st.slider("Experiência em Cloud", 0, 10)
